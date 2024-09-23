e28a0643d0c2bc1040b7ac947e8dc43bf33c55cd
lsy <lsy22@vip.qq.com>
2024-09-23 13:43:09 +0800

    修复安装脚本

--- Menus/Install.sh
+++ Menus/Install.sh
@@ -9,12 +9,15 @@ fi
 mkdir -p "$path"
 rm -rf "$path/linuxtoo"
 
-wget https://g.lsy22.com/lsy/LinuxTool/archive/master.tar.gz -O "${path}/aa"
+wget https://g.lsy22.com/lsy/LinuxTool/archive/master.tar.gz -O "${path}/aa" &> /dev/null
 tar -zxf "$path/aa" -C $path
 rm "$path/aa"
 chmod +x "$path/linuxtool/Run.sh"
 
 sed -i '/alias tool.*/d' "${HOME}/.bashrc"
-echo "alias tool=$path/linuxtool/Run.sh" | cat >> "${HOME}/.bashrc"
+echo "alias tool='$path/linuxtool/Run.sh'" | cat >> "${HOME}/.bashrc"
 
 source "${HOME}/.bashrc"
+echo "工具箱已经安装成功"
+echo "位置：${path}/linuxtool"
+echo "命令：tool"
