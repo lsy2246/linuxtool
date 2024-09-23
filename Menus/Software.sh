88fa604f7d68bc6fac9e649b36e8a6ded440d377
lsy <lsy22@vip.qq.com>
2024-09-23 20:17:45 +0800

    修复软件安装正则问题

--- Menus/Software.sh
+++ Menus/Software.sh
@@ -56,9 +56,13 @@ if [[ -z $pick ]];then
           pick="$i"
       fi
   done
-elif ! [[ $pick =~ [1-${#soft_dick[@]}\s]+ ]];then
-  echo "输入错误"
-  exit
+else
+  for i in $pick ; do
+      if [[ $i =~ [1-${#soft_dick[@]}] ]]; then
+          echo "输入错误"
+          exit
+      fi
+  done
 fi
 
 for i in $pick ; do
