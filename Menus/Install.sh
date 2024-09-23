af9cc15020d2aa6a5df6db3917192187e21cfa54
lsy <lsy22@vip.qq.com>
2024-09-23 13:48:20 +0800

    修复安装脚本

--- Menus/Install.sh
+++ Menus/Install.sh
@@ -9,10 +9,10 @@ fi
 mkdir -p "$path"
 rm -rf "$path/linuxtoo"
 
-wget https://g.lsy22.com/lsy/LinuxTool/archive/master.tar.gz -O "${path}/aa" &> /dev/null
-tar -zxf "$path/aa" -C $path
+wget https://g.lsy22.com/lsy/LinuxTool/archive/master.tar.gz -O "${path}/aa" &> /dev/null || echo "脚本下载失败" && exit
+tar -zxf "$path/aa" -C $path &> /dev/null || echo "脚本解压" && exit
 rm "$path/aa"
-chmod +x "$path/linuxtool/Run.sh"
+chmod +x "$path/linuxtool/Run.sh" &> /dev/null || echo "脚本添加执行权限失败" && exit
 
 sed -i '/alias tool.*/d' "${HOME}/.bashrc"
 echo "alias tool='$path/linuxtool/Run.sh'" | cat >> "${HOME}/.bashrc"
